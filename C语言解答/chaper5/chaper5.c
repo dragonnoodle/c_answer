63c121440d5a7a6c2a126685d33db14fd119c44b
dragonnoodle <31583383+dragonnoodle@gmail.com>
2018-05-21 17:33:25 +0800

    chaper5 changes
    chapter5 change test bitmap

--- C语言解答/chaper5/chaper5.c
+++ C语言解答/chaper5/chaper5.c
@@ -6,7 +6,7 @@
 int
 main()
 {
-#if 1
+#if 0
     int i= -2;
         printf("before right shift i :0x%x\n", i);
     i = i>>1;
@@ -36,27 +36,70 @@ while((ch = getchar()) != EOF )
     
 }
 #endif
-unsigned int flag = 0, count = 0;
-int value;
+
+
+#if 0
+unsigned int flag = 0, flag2 =0, count = 0;
+int value, temp = 0;
 char str_value[64];
 while((gets(str_value)) != NULL )
 {
     value = atoi(str_value);
     printf("before shift :0x%x\n", value);
     flag = (unsigned int)1<<(sizeof(int)*8-1);
-    printf("flag:0x%x\n");
+    flag2 = 1;
+    printf("flag:0x%x\n", flag);
+    count = 0;
+    temp = 0 ;
     while(flag)
     {
         if(flag & value)
+            temp = temp | flag2;
+        else 
+        {
+            if(temp == 0)
             count++;
+        }
         flag = flag >>1;
+        flag2 = flag2 <<1;
+        printf("flag1:0x%x\n", flag);
+        printf("flag2:0x%x\n", flag2);
+        printf("countt:%d\n", count);
     }
+    value = value << count;
     printf("after shift :0x%x\n", value);
+    printf("after symmetry :0x%x\n", temp);
 }
 
+#endif
+
+#if 1
+
+typedef struct bits_map_struct
+{
+    struct bits_map_struct *Pbits_map_self;
+    unsigned int buf_len;
+    unsigned int max_bit_num;
+    unsigned int pos;
+    unsigned int gap_len;
+    unsigned int index;
+    unsigned int num;
+    unsigned int buf[0];
+} bit_Map, *Pbit_Map;
+
+
+Pbit_Map *bit_create(num_bits);
+int bit_destroy(Pbit_Map *bit_Map_name);
+int bit_set(Pbit_Map *bit_Map_name,unsigned int bit_number_pos,unsigned int bit_value);
+int bit_get(Pbit_Map *bit_Map_name,unsigned int bit_number_pos);
+int set_bit_map(Pbit_Map *bit_Map_name,unsigned int bit_value);
 
 
 
+
+#endif
+
+return 0;
     
 }
 
