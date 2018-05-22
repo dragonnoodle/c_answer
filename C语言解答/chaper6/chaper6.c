992f416f5597288bec5ad9304959b970e44370ed
dragonnoodle <31583383+dragonnoodle@gmail.com>
2018-05-22 13:38:42 +0800

    add chapter6
    add chapter6
    1.find_string

---
+++ C语言解答/chaper6/chaper6.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int 
+main()
+{
+#if 0
+char* 
+find_chars(char *source, char *chars)
+{
+    char *temp = NULL, *ch1 = source, *ch2 = chars;
+    int flag = 0;
+    
+    if(ch1 == NULL || ch2 == NULL)
+        return NULL;
+    while( *ch2 != '\0')
+    {
+        for(ch1 = source; *ch1 != '\0'; ch1++)
+        {
+            if(*ch1 == *ch2)
+            {
+                printf("ch1:%c\n", *ch1);
+                return ch1;
+            }
+        }
+        ch2++;
+    }
+    return NULL;
+}
+
+
+int *ch;
+char input1[64], input2[64];
+
+while( gets(input1) && gets(input2) )
+{
+    printf(" input1:%s \n input2:%s\n ", input1, input2);
+    ch = find_chars(input1, input2);
+}
+
+return 0;
+#endif
+
+
+
+#if 1
+char*
+reverse_string(char* input)
+{
+    char *ch1 = input;
+    char temp;
+    int len, i =0;
+    len = strlen(ch1);
+    
+    while(*ch1 != '\0')
+    {   
+        if(len %2 == 0)
+        if(len/2 >= i)
+        temp = *(input+i);
+        *(input+i) = *(input +len-1);
+        *(input +len-1) = temp;
+
+    }
+    
+    
+    
+}
+
+
+#endif
+
+
+
+}
